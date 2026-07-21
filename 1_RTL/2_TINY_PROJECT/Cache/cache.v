1f340f3a52ff22cc374d7918e1ce3d6b7e50383d
seanok <okhouse1105@gmail.com>
2026-07-22 02:08:03 +0900

    update

--- 1_RTL/2_TINY_PROJECT/Cache/cache.v
+++ 1_RTL/2_TINY_PROJECT/Cache/cache.v
@@ -1,6 +1,6 @@
 module cache(
 
-i_cahe_req,
+i_cache_req,
 i_cache_write,
 i_cache_address,
 i_cpu_wdata,
@@ -22,6 +22,20 @@ input [15:0] i_cpu_wdata;
 output o_cpu_ack;
 output [15:0] o_cpu_rdata;
 
-reg [15:0] cache [7:0];
+reg [15:0] cache [7:0]; // I need to make it  SRAM module later.
+
+reg [7:0] valid;
+
+wire [1:0] w_tag; 
+wire [2:0] w_index;
+wire [15:0] data_rd;
+
+
+wire w_cpu_read = i_cache_req & o_cpu_ack & ~(i_cache_write);
+
+wire hit = (w_cpu_read & valid) &&( w_tag == i_cache_address [5:3]);
+
+assign o_cpu_rdata = hit ? data_rd : 
+
 
 endmodule
