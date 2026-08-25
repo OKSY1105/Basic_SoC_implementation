64b05b0302e724572052f1de124d5e9255c70be2
OK Seo YUN <okhouse1105@gmail.com>
2026-08-25 20:56:15 +0900

    Update

--- 1_RTL/2_TINY_PROJECT/Cache/cache.v
+++ 1_RTL/2_TINY_PROJECT/Cache/cache.v
@@ -1,41 +1,44 @@
 module cache(
 
-i_cache_req,
-i_cache_write,
-i_cache_address,
-i_cpu_wdata,
-o_cpu_ack,
-o_cpu_rdata,
+i_clk,
+i_rstn,
+i_cpu_req, // cpu request
+i_cache_write, // cpu is going to write data to cache
+i_cpu_memaddr, // cpu give memory address to cache
+i_cpu_wdata, // cpu is going to write wdata
+o_cpu_ack, // cache tell cpu i finish work that you request
+o_cpu_rdata, // cpu read data
 
-o_mem_req,
-o_mem_write,
-o_mem_address,
-o_mem_wdata,
-i_mem_ack,
-i_mem_rdata
+o_mem_req, // miss cache reqest data to memory
+o_mem_write, // cache data is going to written to memory
+o_mem_address, //cache give  memory address 
+o_mem_wdata, // cahce is going to write wdata to memory
+i_mem_ack, // memory tell cache i finish work that you request
+i_mem_rdata // data is written to cache
 
 );
-input i_cache_req;
+input i_cpu_req;
 input i_cpu_write;
-input [5:0] i_cache_address;
+input [7:0] i_cpu_memaddr;
 input [15:0] i_cpu_wdata;
 output o_cpu_ack;
 output [15:0] o_cpu_rdata;
 
 reg [15:0] cache [7:0]; // I need to make it  SRAM module later.
-
 reg [7:0] valid;
 
-wire [1:0] w_tag; 
-wire [2:0] w_index;
-wire [15:0] data_rd;
+wire [1:0] w_tag;     // i_cpu_memaddr - index - offset 
+wire [2:0] w_index;  // size of cache
+wire [1:0] w_offset; //size of byte
+reg [1:0] r_cache_tag;     // i_cpu_memaddr - index - offset
+reg [2:0] r_cache_index;  // size of cache
+reg [1:0] r_cache_offset; //size of byte
 
 
-wire w_cpu_read = i_cache_req & o_cpu_ack & ~(i_cache_write);
+wire hit;
+wire miss;
 
-wire hit = (w_cpu_read & valid) &&( w_tag == i_cache_address [5:3]);
-
-assign o_cpu_rdata = hit ? data_rd : 
+assign hit = i_cpu_req & valid &(r_cache_tag == w_tag); 
 
 
 endmodule
